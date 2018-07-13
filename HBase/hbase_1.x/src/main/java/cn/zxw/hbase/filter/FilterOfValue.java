efdfc5cff8dd81b3a6c565990cc8223db2255a4d
zhang xiangwei <zhangxw17@lenovo.com>
2018-07-13 18:26:41 +0800

    add hbase filter

---
+++ HBase/hbase_1.x/src/main/java/cn/zxw/hbase/filter/FilterOfValue.java
@@ -0,0 +1,48 @@
+package cn.zxw.hbase.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.filter.BinaryComparator;
+import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.FilterList;
+import org.apache.hadoop.hbase.filter.ValueFilter;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import cn.zxw.hbase.util.HBaseConnectUtil;
+
+public class FilterOfValue {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		try {
+			Table table=HBaseConnectUtil.getTable("test");
+			//Filter filter = new ValueFilter(CompareOp.EQUAL,new SubstringComparator("103"));
+			List<Filter> filters = new ArrayList<Filter>(2);
+			Filter filter1 = new ValueFilter(CompareOp.GREATER,new BinaryComparator("103".getBytes()));
+			Filter filter2 = new ValueFilter(CompareOp.LESS,new BinaryComparator("106".getBytes()));
+			filters.add(filter1);
+			filters.add(filter2);
+			Scan scan = new Scan();
+	        scan.setFilter(new FilterList(filters));
+	        ResultScanner scanner = table.getScanner(scan);
+	        for (Result result : scanner){
+	            for (Cell cell:result.rawCells()){
+	                System.out.println("Cell: "+Bytes.toString(cell.getFamily())+":"
+	                		+Bytes.toString(cell.getQualifier())
+	                		+", Value: "+Bytes.toString(cell.getValue()));
+	            }
+	        }
+	        scanner.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
